043138989b76239c2700150c928f073299d8df1b
Tom Webber <t.c.w.webber@gmail.com>
2024-05-04 17:53:42 +0200

    [Add][DB] Configuration and display content

---
+++ src/schema.rs
@@ -0,0 +1,12 @@
+#![allow(clippy::restriction)]
+
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    posts (id) {
+        id -> Integer,
+        title -> Text,
+        body -> Text,
+        published -> Bool,
+    }
+}
